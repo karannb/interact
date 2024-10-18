54caf870151dbc003edc141616a8dee866b576a9
Karan Bania <101618474+karannb@users.noreply.github.com>
2024-10-18 16:06:01 +0530

    added metrics and saving tags.

--- src/interact.py
+++ src/interact.py
@@ -29,19 +29,29 @@ def Interact(data, h: int, m: int, n: int, k: int = 3) -> List:
     human = Human(h)
     machine = Machine(m)
 
+    # metrics
+    total_sessions = 0
+    one_way_human, one_way_machine = 0, 0
+    two_way_human, two_way_machine = 0, 0
+    l_m_revision = False
+
     # Iterate over all input data
     for idx, x in data:
         # Generate a random session identifier and store the input data
         sess = uuid.uuid4().hex[:4]
+        total_sessions += 1
         D.append((x, sess))
 
         j = 0
+        tags = []
         done = False
         while not done:
             # ask the machine
             mu_m, C = machine.ask(j, k, (D, M, C)) # (tag, pred, expl) and context
             M += [(sess, j, m, mu_m, h)]
             j += 1
+            if mu_m[0] == "revise":
+                l_m_revision = True
 
             # ask the human
             mu_h, C = human.ask(j, k, (D, M, C))
@@ -50,14 +60,28 @@ def Interact(data, h: int, m: int, n: int, k: int = 3) -> List:
 
             # stopping condition
             done = (j > n) or (mu_h[0] == "ratify") or (mu_h[0] == "reject")
+            tags.extend([f"Machine: {mu_m[0]}", f"Human: {mu_h[0]}"])
 
         # only check for ratify because, in this special case,
         # human agent can never revise.
+        l_h, l_m = mu_h[0], mu_m[0]
         if mu_h[0] == "ratify":
-            print("Strongly intelligible for the Human.")
-        elif mu_h[0] == "reject" or mu_h[0] == "refute":
-            print("Not Strongly intelligible for the Human.")
+            one_way_human += 1
+        if mu_m[0] == "ratify" or l_m_revision:
+            one_way_machine += 1
+        if (mu_h[0] == "ratify") and (mu_m[0] == "ratify" or l_m_revision):
+            two_way_human += 1
+            two_way_machine += 1
 
+        # store the tags
+        with open("tags.txt", "a") as f:
+            f.write(f"sessionID-{sess} ::: tags-{tags}\n")
+
+    print(f"Total Sessions: {total_sessions}")
+    print(f"One-way Human: {one_way_human}")
+    print(f"One-way Machine: {one_way_machine}")
+    print(f"Two-way Human: {two_way_human}")
+    print(f"Two-way Machine: {two_way_machine}")
     return D, M, C
 
 
